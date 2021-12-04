697d65e2ce57dc22b89060bdc7a91ada9d068242
Ryan Neph <neph320@gmail.com>
2021-12-04 09:43:06 -0800

    templates/rust: put main() at end of file

--- 2021/templates/rust/src/main.rs
+++ 2021/templates/rust/src/main.rs
@@ -1,14 +1,5 @@
 use std::fs;
 
-fn main() {
-    let filename = "../input.txt";
-    let contents = fs::read_to_string(filename)
-        .expect("Failed to read the file");
-
-    part1(&contents);
-    part2(&contents);
-}
-
 fn part1(contents: &String) {
     for line in contents.lines() {
         println!("{}", line);
@@ -23,4 +14,13 @@ fn part2(contents: &String) {
     }
 
     println!("PART2: {}", 0);
+}
+
+fn main() {
+    let filename = "../input.txt";
+    let contents = fs::read_to_string(filename)
+        .expect("Failed to read the file");
+
+    part1(&contents);
+    part2(&contents);
 }
