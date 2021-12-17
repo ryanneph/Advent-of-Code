d788f32c0d6593d0942d9fbaf3e0a16f897603b3
Ryan Neph <neph320@gmail.com>
2021-12-17 11:10:18 -0800

    templates: update rust template

--- 2021/templates/rust/src/main.rs
+++ 2021/templates/rust/src/main.rs
@@ -1,27 +1,18 @@
 use std::fs;
 
-fn part1(contents: &String) {
-   for line in contents.lines() {
-      println!("{}", line);
-   }
-
+fn part1() {
    println!("PART1: {}", 0);
 }
 
-fn part2(contents: &String) {
+fn main() {
+   // let filename = "../input.txt";
+   let filename = "../test1.txt";
+   let contents = fs::read_to_string(filename)
+      .expect("Failed to read the file");
+
    for line in contents.lines() {
       println!("{}", line);
    }
 
-   println!("PART2: {}", 0);
-}
-
-fn main() {
-   let filename = "../input.txt";
-   // let filename = "../test1.txt";
-   let contents = fs::read_to_string(filename)
-      .expect("Failed to read the file");
-
-    part1(&contents);
-    part2(&contents);
+    part1();
 }
